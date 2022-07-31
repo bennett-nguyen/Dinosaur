f54e95cb9b3776aeca16dd565f2a38a11d834784
bennett-nguyen <83117848+bennett-nguyen@users.noreply.github.com>
2022-07-31 12:08:53 +0700

    Introduce new constants for manipulating the game

--- src/preload/constant.py
+++ src/preload/constant.py
@@ -1,23 +1,36 @@
 """
 Defines constants that are used throughout the game
 """
-WIDTH = 1200
-HEIGHT = 500
-FPS = 60
-PAUSED_FPS = 30
+# doing private imports btw
+from collections import namedtuple as __namedtuple
 
-DINO_POS_X_OFFSET = 40
-DINO_POS_Y_OFFSET = 20
-GROUND_POS_Y_OFFSET = 30
+__RGB = __namedtuple('RGB', ('R', 'G', 'B'))
 
-HALF_WIDTH = WIDTH/2
-HALF_HEIGHT = HEIGHT/2
+WIDTH: int = 1200
+HEIGHT: int = 500
+FPS: int = 60
+PAUSED_FPS: int = 30
 
-DAY_SCREEN_COLOR = (255, 255, 255)
-NIGHT_SCREEN_COLOR = (0, 0, 0)
+HALF_WIDTH: int = WIDTH/2
+HALF_HEIGHT: int = HEIGHT/2
 
-DAY_MESSAGE_COLOR = (82, 82, 83)
-NIGHT_MESSAGE_COLOR = (255, 255, 255)
+DINO_POS_X_OFFSET: int = 40
+DINO_POS_Y_OFFSET: int = 20
+GROUND_POS_Y_OFFSET: int = 30
 
-STAR_ALPHA = 150
-MOON_ALPHA = 255
+
+DAY_SCREEN_COLOR = __RGB(255, 255, 255)
+NIGHT_SCREEN_COLOR = __RGB(0, 0, 0)
+
+DAY_MESSAGE_COLOR = __RGB(82, 82, 83)
+NIGHT_MESSAGE_COLOR = __RGB(255, 255, 255)
+
+STAR_ALPHA: int = 255
+MOON_ALPHA: int = 255
+
+CLOUDS_AMOUNT: int = 6
+STARS_AMOUNT: int = 10
+
+DAY_NIGHT_CYCLE_DELAY: float | int = 7 # in seconds
+
+DINO_VELOCITY = 700
