83dc3d3f4524f1fa847f8de83dd57a94720b7b24
bennett-nguyen <83117848+bennett-nguyen@users.noreply.github.com>
2022-07-31 11:58:55 +0700

    Include dinosaur game icon

---
+++ assets/img/icon/readme.md
@@ -0,0 +1,2 @@
+An icon image file should be 32x32px
+Ảnh icon phải là 32x32px
--- src/preload/ds.py
+++ src/preload/ds.py
@@ -5,8 +5,12 @@ import src.preload.constant as const
 pg.mixer.pre_init(44100, -16, 2, 512)
 pg.init()
 
+
 pg.event.set_allowed([QUIT, KEYDOWN, KEYUP, MOUSEBUTTONDOWN])
 pg.display.set_caption("Python Dinosaur")
 
 clock = pg.time.Clock()
 screen = pg.display.set_mode((const.WIDTH, const.HEIGHT))
+
+__ICON = pg.image.load('./assets/img/icon/dino.ico').convert_alpha()
+pg.display.set_icon(__ICON)
