da85140597e44f015a255b371d05212ca75136cf
bennett-nguyen <83117848+bennett-nguyen@users.noreply.github.com>
2022-08-08 16:31:22 +0700

    Add more events to check

--- src/preload/ds.py
+++ src/preload/ds.py
@@ -1,12 +1,12 @@
 import pygame as pg
-from pygame.locals import QUIT, KEYDOWN, KEYUP, MOUSEBUTTONDOWN
+from pygame.locals import QUIT, KEYDOWN, KEYUP, MOUSEBUTTONDOWN, WINDOWFOCUSLOST, WINDOWMOVED
 import src.preload.constant as const
 
 pg.mixer.pre_init(44100, -16, 2, 512)
 pg.init()
 
 
-pg.event.set_allowed([QUIT, KEYDOWN, KEYUP, MOUSEBUTTONDOWN])
+pg.event.set_allowed([QUIT, KEYDOWN, KEYUP, MOUSEBUTTONDOWN, WINDOWFOCUSLOST, WINDOWMOVED])
 pg.display.set_caption("Python Dinosaur")
 
 clock = pg.time.Clock()
