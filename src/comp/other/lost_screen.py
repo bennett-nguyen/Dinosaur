1e0ef623c7926199fd804c43dc8ad44ddef526d8
bennett-nguyen <83117848+bennett-nguyen@users.noreply.github.com>
2022-08-10 16:36:39 +0700

    Introduce lost screen

---
+++ src/comp/other/lost_screen.py
@@ -0,0 +1,15 @@
+import pygame as pg
+import src.preload.assets as assets
+from src.preload.shared import shared_data, cache
+from src.comp.export.sub_process import Process
+
+class LostScreen(Process):
+    def __init__(self, _f):
+        super().__init__('G A M E  O V E R', assets.Gallery.RETRY_BUTTON)
+        self._reset_function = _f
+
+    def on_click_event(self):
+        shared_data.distance_incrementer = shared_data.velocity_incrementer = shared_data.paused_delay = 0
+        shared_data.time_state = cache.time_state
+        self._reset_function()
+        shared_data.is_lose = False
