1db23ad76e2ba4faae0e0ffc759e831de5fcec86
Bennett Nguyễn <7akk4r2821@gmail.com>
2023-03-23 22:03:11 +0700

    Move everything to game.py

--- entry.py
+++ entry.py
@@ -1,35 +1,4 @@
-import time
-import pygame as pg
-import src.preload.ds as ds
-import src.preload.constant as const
+from src.comp.main.game import Game
 
-from sys import exit
-from src.comp.scene.game import Game
-from src.preload.shared import shared_data
-
-main_game = Game()
-
-dt: float = 0
-current: float = 0
-last: float = time.time()
-
-while 1:
-    current = time.time()
-    dt = current - last
-    last = current
-
-    ds.screen.fill(main_game.screen_color)
-    main_game.apply_color()
-    ds.clock.tick(const.FPS)
-
-    events = pg.event.get()
-    shared_data.events = events
-    shared_data.dt = dt
-
-    for event in events:
-        if event.type == pg.QUIT:
-            pg.quit()
-            exit()
-
-    main_game.update()
-    pg.display.flip()
+game = Game()
+game.run()
